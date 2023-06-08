3e58ee595907720fd8dc67213c378348741f1325
KilonzoJames <jay.kilonzo@gmail.com>
2023-06-08 17:01:17 +0300

    Arithmetic lab

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+     //Number 1
+    }
+function subtract(){}
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n){
+    return (n+=1);
+}
+function decrement(n){
+    return (n-=1);
+}
+
+function subtract1(n){
+    return( n-=1);
+}
+subtract1;
+function makeInt(string){
+    return parseInt,10;
+}
+function preserveDecimal(string){
+    return parseInt(string,10);
+}
+function makeInt(string){
+    return parseInt(string,10);
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+}
+
